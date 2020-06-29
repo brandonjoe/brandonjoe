896dd425ef125d1cd03b7a2dd7ecc1f12552930c
BrandonJoe <brandon.joe42@yahoo.com>
2020-06-29 12:48:27 -0700

    added new darts

--- src/Portfolio/Portfolio.js
+++ src/Portfolio/Portfolio.js
@@ -7,7 +7,7 @@ import port1 from '../media/port1.jpg';
 import port2 from '../media/port2.JPG';
 import port3 from '../media/port3.JPG';
 import port4 from '../media/port4.JPG';
-import port5 from '../media/port5.JPG';
+import port5 from '../media/port5.jpg';
 import port6 from '../media/port6.JPG';
 import port7 from '../media/port7.JPG';
 import port8 from '../media/port8.JPG';
@@ -34,10 +34,13 @@ const Portfolio = (props) => {
                     <Project img={port2} title={'Sphere Game'} demo={'https://github.com/brandonjoe/SphereGame'} languages={'Unity, C#'} github={'https://github.com/brandonjoe/SphereGame'} text={'Physics based ball game developed in Unity 3D with C#.'} className={classes.project}/>
                     </ScrollAnimation>
                     <ScrollAnimation  animateOnce={true} duration={2} animateIn='fadeIn' className={classes.project}>
-                    <Project img={port10} title={'VMX Convention'} demo={'https://github.com/brandonjoe/VMXbanners'} languages={'HTML, CSS, Javascript'} github={'https://github.com/brandonjoe/VMXbanners'} text={'Banners Ads for VMX Vet Convention'} className={classes.project}/>
+                    <Project img={port10} title={'HTML5 banner'} demo={'https://github.com/brandonjoe/VMXbanners'} languages={'HTML, CSS, Javascript'} github={'https://github.com/brandonjoe/VMXbanners'} text={'Banners Ads for VMX Vet Convention'} className={classes.project}/>
                     </ScrollAnimation>
                     <ScrollAnimation  animateOnce={true} duration={2} animateIn='fadeIn'className={classes.project}>
-                    <Project img={port7} title={'Steam News'} demo={'https://brandonjoe.github.io/SteamApp/'} languages={'React, Node/Express'} github={'https://github.com/brandonjoe/SteamApp'} text={'Generates current news for Steam games you play.'} className={classes.project}/>
+                    <Project img={port5} title={'Sovalineups.com'} demo={'https://sovalineups.com/'} languages={'HTML/CSS, React'} github={'https://github.com/brandonjoe/store'} text={'App for Valorant to help player lineups'} className={classes.project}/>
+                    </ScrollAnimation>
+                    <ScrollAnimation  animateOnce={true} duration={2} animateIn='fadeIn'className={classes.project}>
+                    <Project img={port7} title={'Steamnews.info'} demo={'http://steamnews.info/'} languages={'React, Node/Express'} github={'https://github.com/brandonjoe/SteamApp'} text={'Generates current news for Steam games you play.'} className={classes.project}/>
                     </ScrollAnimation>
                     <ScrollAnimation  animateOnce={true} duration={2} animateIn='fadeIn'className={classes.project}>
                     <Project img={port3} title={'E-Commerce'} demo={'https://brandonjoe.github.io/store/'} languages={'HTML/CSS, React'} github={'https://github.com/brandonjoe/store'} text={'Fully functional E-Commerce store based off an Etsy Store.'} className={classes.project}/>
